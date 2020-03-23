015dade31e27f60b4b6ce6c23f1be60bb36a107a
Alexey-Koren <johnsmitlpf@gmail.com>
2020-03-23 13:45:19 +0300

    refactor: avg function

--- src/index.js
+++ src/index.js
@@ -7,5 +7,5 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-  return array !== undefined && array.reduce((sum, current) => sum + current, 0) ? array.reduce((sum, current) => sum + current, 0) / array.length : 0;
+  return array !== undefined && array.length > 0 ? array.reduce((sum, current) => sum + current, 0) / array.length : 0;
 }
